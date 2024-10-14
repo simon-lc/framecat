0a868d319b1a395e9bd9f9b5a960d563d6070bcc
simon-lc <simonlc@stanford.edu>
2024-10-14 19:59:05 -0400

    docs

---
+++ docs/tree_gh.py
@@ -0,0 +1,91 @@
+# .
+# ├── .buildinfo
+# ├── .doctrees
+# │   ├── docs.doctree
+# │   ├── environment.pickle
+# │   ├── index.doctree
+# │   ├── quickstart.doctree
+# │   └── reference
+# │       └── api
+# │           ├── cli_interface.doctree
+# │           ├── index.doctree
+# │           └── rendering.doctree
+# ├── _images
+# │   ├── logo-dark.svg
+# │   └── logo-light.svg
+# ├── _modules
+# │   ├── framecat
+# │   │   ├── cli_interface.html
+# │   │   └── rendering.html
+# │   └── index.html
+# ├── _sources
+# │   ├── docs.md.txt
+# │   ├── index.md.txt
+# │   ├── quickstart.md.txt
+# │   └── reference
+# │       └── api
+# │           ├── cli_interface.rst.txt
+# │           ├── index.rst.txt
+# │           └── rendering.rst.txt
+# ├── _sphinx_design_static
+# │   ├── design-style.b7bb847fb20b106c3d81b95245e65545.min.css
+# │   └── design-tabs.js
+# ├── _static
+# │   ├── _sphinx_javascript_frameworks_compat.js
+# │   ├── basic.css
+# │   ├── check-solid.svg
+# │   ├── clipboard.min.js
+# │   ├── copy-button.svg
+# │   ├── copybutton.css
+# │   ├── copybutton.js
+# │   ├── copybutton_funcs.js
+# │   ├── debug.css
+# │   ├── design-style.b7bb847fb20b106c3d81b95245e65545.min.css
+# │   ├── design-tabs.js
+# │   ├── doctools.js
+# │   ├── documentation_options.js
+# │   ├── file.png
+# │   ├── images
+# │   │   ├── logo-dark.png
+# │   │   ├── logo-dark.svg
+# │   │   ├── logo-light.png
+# │   │   └── logo-light.svg
+# │   ├── jquery-3.6.0.js
+# │   ├── jquery.js
+# │   ├── language_data.js
+# │   ├── minus.png
+# │   ├── mystnb.css
+# │   ├── plus.png
+# │   ├── pygments.css
+# │   ├── scripts
+# │   │   ├── furo-extensions.js
+# │   │   ├── furo.js
+# │   │   ├── furo.js.LICENSE.txt
+# │   │   └── furo.js.map
+# │   ├── searchtools.js
+# │   ├── skeleton.css
+# │   ├── sphinx_highlight.js
+# │   ├── styles
+# │   │   ├── furo-extensions.css
+# │   │   ├── furo-extensions.css.map
+# │   │   ├── furo.css
+# │   │   └── furo.css.map
+# │   ├── togglebutton.css
+# │   ├── togglebutton.js
+# │   ├── twemoji.css
+# │   ├── twemoji.js
+# │   ├── underscore-1.13.1.js
+# │   └── underscore.js
+# ├── docs.html
+# ├── genindex.html
+# ├── index.html
+# ├── objects.inv
+# ├── py-modindex.html
+# ├── quickstart.html
+# ├── reference
+# │   └── api
+# │       ├── cli_interface.html
+# │       ├── index.html
+# │       └── rendering.html
+# ├── search.html
+# └── searchindex.js
